1d476b2a19435d636c68562ede1178a1ad0d9ae5
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2023-04-21 20:59:26 +0900

    delete project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pokedict
+포켓몬 도감 앱 클론코딩
+
+<img width="524" alt="image" src="https://user-images.githubusercontent.com/111988634/233626175-4179fa70-c629-425d-842d-7a7093ad3c44.png">
--- toonflix/lib/models/webtoon_episode_model.dart
+++
@@ -1,9 +0,0 @@
-class WebtoonEpisodeMdoel {
-  final String id, title, rating, date;
-
-  WebtoonEpisodeMdoel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        title = json['title'],
-        rating = json['rating'],
-        date = json['date'];
-}
